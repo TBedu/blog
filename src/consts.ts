d859bc9659ff20143fdd4ce3a6ce96a3dd262963
TBedu <2877181146@qq.com>
2024-12-27 19:39:17 +0800

    配置评论系统+更改语言

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // Site title and description
-export const SITE_LANG = "en";
+export const SITE_LANG = "zh";
 export const SITE_TAB = "Frosti";
 export const SITE_TITLE = "Frosti 🧊";
 export const SITE_DESCRIPTION = "A blog template";
@@ -14,7 +14,7 @@ export const USER_SITE = "https://frosti.saroprock.com"
 export const USER_AVATAR = "/profile.webp";
 
 // Server and transition settings
-export const SERVER_URL = "https://demo.saroprock.com";
+export const SERVER_URL = "https://waline.tbedu.top";
 
 // Theme settings
 export const DAISYUI_THEME = {
